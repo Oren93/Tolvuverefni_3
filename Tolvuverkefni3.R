48d3d5915b55d1b4dc28d60bea05754279f8b8a9
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-03-04 18:04:42 +0000

    bla

--- Tolvuverkefni3.R
+++ Tolvuverkefni3.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(readr)
 library(dplyr)
-library(lubridate) # pakki med follum fyrir vinnslu med dagsetningar (date eda datetime breytur)
+library(lubridate)
 library(ggplot2)
 oo <- read.csv(file = 'data98.csv', sep =',')
+oo <- subset(oo, select = -c(vf))
 oo = oo %>% rename("a" = "recid",
                    "b" = "reit",
                    "c" = "smrt",
@@ -12,7 +13,6 @@ oo = oo %>% rename("a" = "recid",
                    "f" = "man",
                    "g" = "dyp_min",
                    "h" = "dyp_max",
-                   "i" = "vf",
                    "j" = "nr",
                    "k" = "le",
                    "l" = "ky",
@@ -21,6 +21,11 @@ oo = oo %>% rename("a" = "recid",
                    "o" = "osl",
                    "p" = "sl",
                    "q" = "li")
+oo <- oo %>% mutate(season = recode_factor(month, "1"="winter", "2"="winter",
+                                           "12"="winter", "3"="spring",
+                                           "4"="spring","5"="spring","6"="summer","7"="summer",
+                                           "8"="summer","9"="autumn","10"="autumn","11"="autumn"))
+
 
 r2d <-function(r)
 {
