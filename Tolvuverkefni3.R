ac22cf83298db87531b7891c4568ecf183b3e345
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-03-04 17:51:02 +0000

    Create Tolvuverkefni3.R

---
+++ Tolvuverkefni3.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(lubridate) # pakki med follum fyrir vinnslu med dagsetningar (date eda datetime breytur)
+library(ggplot2)
+oo <- read.csv(file = 'data98.csv', sep =',')
+oo = oo %>% rename("a" = "recid",
+                   "b" = "reit",
+                   "c" = "smrt",
+                   "d" = "tog_nr",
+                   "e" = "dag",
+                   "f" = "man",
+                   "g" = "dyp_min",
+                   "h" = "dyp_max",
+                   "i" = "vf",
+                   "j" = "nr",
+                   "k" = "le",
+                   "l" = "ky",
+                   "m" = "kt",
+                   "n" = "aldur",
+                   "o" = "osl",
+                   "p" = "sl",
+                   "q" = "li")
+
+r2d <-function(r)
+{
+    lat <- floor(r/100)
+    lon <- (r - lat * 100) %% 50
+    halfb <- (r - 100 * lat - lon)/100
+    lon <-  - (lon + 0.5)
+    lat <- lat + 60 + halfb + 0.25
+    data.frame(lat = lat, lon = lon)
+}
+reitir<-unique(oo$reit)
+x<-r2d(reitir)$lon
+y<-r2d(reitir)$lat
+plot(x,y,type='n')
+text(x,y,as.character(reitir))
