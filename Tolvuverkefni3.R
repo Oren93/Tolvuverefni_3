cd5286e12ed8b77c08b5419b6b3a15f9eb3556f8
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-03-04 18:43:22 +0000

    4.3.20 18:45

--- Tolvuverkefni3.R
+++ Tolvuverkefni3.R
@@ -4,22 +4,16 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 oo <- read.csv(file = 'data98.csv', sep =',')
-oo <- subset(oo, select = -c(vf))
-oo = oo %>% rename("a" = "recid",
-oo <- oo %>% mutate(season = recode_factor(month, "1"="winter", "2"="winter",
-                                           "12"="winter", "3"="spring",
-                                           "4"="spring","5"="spring","6"="summer","7"="summer",
-                                           "8"="summer","9"="autumn","10"="autumn","11"="autumn"))
-
-
-                   "area" = "reit",
+oo <- subset(oo, select = -c(recid,vf))
+oo <- oo %>% mutate(season = recode_factor(month, "1"="NE", "2"="SE",
+                                           "12"="SW", "3"="NW"))
+oo = oo%>%rename("area" = "reit",
                    "sub_area" = "smrt",
                    "trawl_num" = "tog_nr",
                    "day" = "dag",
                    "month" = "man",
                    "min_depth" = "dyp_min",
                    "max_depth" = "dyp_max",
-                   "equipment" = "vf",
                    "fish_num" = "nr",
                    "fish_length" = "le",
                    "fish_gender" = "ky",
