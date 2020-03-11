2732e71aacf83e31fb2074f0a1a6e6de1ed52979
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-03-11 16:42:21 +0000

    a

--- Tolvuverkefni3.R
+++ Tolvuverkefni3.R
@@ -5,8 +5,7 @@ library(lubridate)
 library(ggplot2)
 oo <- read.csv(file = 'data98.csv', sep =',')
 oo <- subset(oo, select = -c(recid,vf))
-oo <- oo %>% mutate(season = recode_factor(month, "1"="NE", "2"="SE",
-                                           "12"="SW", "3"="NW"))
+# Change the names to more usefull names
 oo = oo%>%rename("area" = "reit",
                    "sub_area" = "smrt",
                    "trawl_num" = "tog_nr",
@@ -32,8 +31,25 @@ r2d <-function(r)
     lat <- lat + 60 + halfb + 0.25
     data.frame(lat = lat, lon = lon)
 }
-reitir<-unique(oo$reit)
-x<-r2d(reitir)$lon
-y<-r2d(reitir)$lat
-plot(x,y,type='n')
-text(x,y,as.character(reitir))
+reitir<-unique(oo$area)
+reitir<-oo$area
+# x<-r2d(reitir)$lon
+# y<-r2d(reitir)$lat
+long <- r2d(reitir)$lon
+lat <- r2d(reitir)$lat
+oo <- cbind(oo, long, lat)
+plot(long,lat,type='n')
+text(long,lat,as.character(reitir))
+
+
+oo <- oo %>% mutate(season = recode_factor(long, lat, "1"="NE", "2"="SE",
+                                           "12"="SW", "3"="NW"))
+oo <- mutate(oo, relative_area = case_when(
+    long >=65 && lat >= -19  ~ "NE",
+    long <65 && lat >= -19  ~ "NW",
+    long <65 && lat < -19  ~ "SW",
+    long >=65 && lat < -19  ~ "SE"
+    ))
+rm(lat,long,reitir,x,y)
+
+
