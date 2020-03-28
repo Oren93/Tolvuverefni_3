355dad7945febfaee04cd0090c1953abaf3d73e2
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-03-28 23:36:19 +0000

    Bonus looks good

--- Tolvuverkefni3.R
+++ Tolvuverkefni3.R
@@ -55,13 +55,12 @@ oo <- mutate(oo, breeding_age2 = case_when(
     breeding_age == 1  ~ FALSE
 ))
 
-# b (not finished)
+# b
 
-#nrow(oo[oo$breeding_age2, oo$quadrant == "SW",])
-#nrow(oo[oo$quadrant = "SE",])
-#nrow(subset(oo, breeding_age2==TRUE,fish_mass>2000))
-
-#
+nrow(oo[oo$breeding_age2, oo$quadrant == "SW",])
+nrow(oo[oo$quadrant = "SE",])
+nrow(subset(oo, breeding_age2==TRUE,fish_mass>2000))
+#)
 NE <- c(nrow(filter(oo, breeding_age2 ==FALSE, quadrant == "NE")),
         nrow(filter(oo, breeding_age2 ==TRUE, quadrant == "NE")))
 NW <- c(nrow(filter(oo, breeding_age2 ==FALSE, quadrant == "NW")),
@@ -98,10 +97,134 @@ for (i in 1: max(age_ordered$age)) {
   sd_by_age <- append(sd_by_age, sd(x$length))
   age <- append(age, nrow(x))
 }
-rm(x)
+rm(x,i)
 
-
+## to fix red dots on average:
 ggplot(age_ordered, aes(x = age, y = length))+
   geom_point()+
   geom_smooth(method = "loess") 
+
+## average table to plot, attempting to fix previous plot
+plot.data <- melt(tapply(age_ordered$length, age_ordered$age,mean), varnames="age", value.name="length")
+
+ggplot(plot.data, aes(x = age, y = length))+
+  geom_point()+
+  geom_smooth(method = "loess") 
+##### NOTE: none of the two chunks above are perfect!
+ggplot(plot.data, aes(x=age,y=length)) + geom_bar(position="dodge", stat="identity")+
+  theme_linedraw() + labs(title="Length of fish by age")
+
+ggplot(plot.data, aes(x=age,y=length)) + geom_bar(position="dodge", stat="identity")+
+  theme_linedraw() + labs(title="Length of fish by age")
+
+# F
+library(reshape2)
+oo_long = melt(oo, id.vars='quadrant', measure.vars='fish_length', value.name='fish_length')
+# Skilgreinum fall sem ad tekur inn gagnavigur x og
+#   skilar gognum sem fylgja "natturulegu" normaldreifingu vigursins x
+get_normal_density <- function(x, binwidth) {
+  grid <- seq(min(x), max(x), length=100)
+  data.frame(
+    fish_length = grid,
+    normal_curve = dnorm(grid, mean(x), sd(x)) * length(x) * binwidth
+  )
+}
+
+# Skilgreinum breytu fyrir binwidth
+BW <- 3
+
+# Buum til normaldreifd gogn fyrir hvert hafsvaedi med thvi ad
+#   beita fallinu "get_normal_density" a lengdarmaelingar sem tilheyra
+#   hverju hafsvaedi fyrir sig
+normaldens <-
+  oo %>%
+  group_by(quadrant) %>%
+  do(get_normal_density(x=.$fish_length, binwidth=BW))
+
+ggplot() + geom_histogram(data=oo_long, aes(x=fish_length),binwidth=BW)+
+  geom_line(data=normaldens,mapping=aes(x=fish_length,y=normal_curve),col="red",size=1)+
+  facet_wrap(~quadrant)+
+  labs(x="length")
+
+rm(age,BW,get_normal_density,C_Vector,sd_by_age,oo_long,normaldens,age_ordered)
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+# the following 2 plot are not to be used
+
+ggplot(oo, aes( x=fish_length)) + 
+  geom_histogram()+xlab("Price (thousands - ISK)")+ylab("Frequency - Number of properties")+
+  labs(title="Length of fish by age",x="length",y="count")+
+  geom_line(stat="count", col = "red")+
+  #geom_line(data = oo, aes(x = fish_length, y = n), col = "red")
+  theme_linedraw()
+
+  ggplot(oo, aes(fish_length)) +
+    geom_histogram(stat="count", position = "dodge") + 
+    geom_line(stat="count", col = "red")+
+    scale_fill_brewer(palette = "Set1")+
+    theme_linedraw()
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+  
+#################################################################
+# Bonus attempt
+  # need to sort out and remove useless libraries
+library(tidyverse)
+library(knitr)    
+library(broom)
+library(stringr)
+library(modelr)
+library(forcats)
+library(ggmap)
+library(maps)
+library(tidyverse)
+library(sf)
+library("ggplot2")
+theme_set(theme_bw())
+library("sf")
+library("sp")
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("rgeos")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+
+## not related. beautiful population size map of the world
+ggplot(data = world) +
+  geom_sf(aes(fill = pop_est)) +
+  scale_fill_viridis_c(option = "plasma", trans = "sqrt")
+## blue map of iceland
+ggplot(data = world) +
+  geom_sf(color = "black", fill = "blue")+
+  coord_sf(xlim = c(min(oo$long), max(oo$long)), ylim = c(min(oo$lat),max(oo$lat)), expand = TRUE)+
+  xlab("Longitude") + ylab("Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$NAME)), " countries)"))
+
+##
+reitir<-unique(oo$area)
+x<-r2d(reitir)$lon
+y<-r2d(reitir)$lat
+sites <- data.frame(longitude=x, latitude = y)
+
+## Icelandic map with fish coordinates (need to beautify, text overlaps)
+ggplot(data = world) +
+  geom_sf(color = "black", fill = "blue")+
+  geom_point(data = sites, aes(x = longitude, y = latitude), size = 4, 
+             shape = 23, fill = "yellow") +
+  xlab("Longitude") + ylab("Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$NAME)), " countries)"))+
+  coord_sf(xlim = c(min(oo$long), max(oo$long)), ylim = c(min(oo$lat),max(oo$lat)), expand = TRUE)+
+#geom_text(data=sites,aes(x=longitude,y=latitude,label=
+#                          paste0("(", x,",",y)),hjust=0, vjust=0)
+geom_text(data=sites,aes(x=longitude,y=latitude,label=unique(oo$area)),hjust=0, vjust=0)
+
+#different approach
+sites <- st_as_sf(sites, coords = c("longitude", "latitude"), 
+                  crs = 4326, agr = "constant")
+
+ggplot(data = world) +
+  geom_sf(color = "black", fill = "blue") +
+  geom_sf(data = sites, size = 4, shape = 23, fill = "red") +
+  ggtitle("Fish around Iceland", subtitle = paste0("(", length(unique(world$NAME)), " countries)"))+
+  coord_sf(xlim = c(min(oo$long), max(oo$long)), ylim = c(min(oo$lat),max(oo$lat)), expand = TRUE)
 
